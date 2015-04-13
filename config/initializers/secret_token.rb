18ad69002f2ea2c4fa13ce66cc7bbd9e2b60b35b
Abhi Ramesh <abhiramesh@gmail.com>
2015-04-13 18:15:09 +0200

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Investing::Application.config.secret_token = '1be0a2ccb69936e246980b04f1db43b923288cc91343356be19c4cbde98611327c3b5115f93dbae342cb66de8f37f45458777aa5eb8074ba23c862dcc0e898f0'
